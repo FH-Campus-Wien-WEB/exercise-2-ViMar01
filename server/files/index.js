5138f25a78730bf64ee17ea2d59424af059e2726
ViMar01 <viktoria.marlovits@gmail.com>
2026-04-02 22:58:09 +0200

    Part 1 done

--- server/files/index.js
+++ server/files/index.js
@@ -2,20 +2,130 @@ window.onload = function () {
   const xhr = new XMLHttpRequest();
   xhr.onload = function () {
     const bodyElement = document.querySelector("body");
+    const mainElement = document.querySelector("main");
     if (xhr.status == 200) {
       const movies = JSON.parse(xhr.responseText);
+
+      function makeList(title, array, div) {
+        const h3 = document.createElement("h3");
+        h3.textContent = title;
+
+        const ul = document.createElement("ul");
+        ul.classList.add("unsortedList");
+        array.forEach(element => {
+          const liste = document.createElement("li");
+          liste.classList.add("liste");
+          liste.textContent = element;
+          ul.append(liste);
+        });
+        div.append(h3, ul);
+      }
+
       for (const movie of movies) {
         /* Task 1.3. Add your code from exercise 1 here 
            and include a non-functional 'Edit' button
            to pass this test */
+
+
+
+        const article = document.createElement("article");
+        article.classList.add("movie");
+
+        const image = document.createElement("img");
+        image.classList.add("poster");
+        image.src = movie.Poster;
+
+        //div damit bild und informationen nebeneinander sein können
+        const informationen = document.createElement("div");
+        informationen.classList.add("informations");
+
+        const titleContainer = document.createElement("div");
+        titleContainer.classList.add("titleContainer");
+        
+        const buttonEdit = document.createElement("button");
+        buttonEdit.classList.add("buttonEdit");
+        buttonEdit.textContent = "Edit";
+        
+        const title = document.createElement("h2");
+        title.classList.add("movieTitel");
+        title.textContent = movie.Title;
+
+        titleContainer.append(title, buttonEdit);
+
+        const plot = document.createElement("p");
+        plot.classList.add("plot");
+        plot.textContent = movie.Plot;
+
+        //contains runtime and release
+        const timeInfo = document.createElement("p");
+        timeInfo.classList.add("timeInformation");
+
+        //contains metascore and imdbRating
+        const rating = document.createElement("p");
+        rating.classList.add("rating");
+
+
+        const released = document.createElement("span");
+        released.classList.add("released");
+        const date = new Date(movie.Released);
+        const formattedDate = date.toLocaleDateString("de-AT");
+        released.textContent = `Released on ${formattedDate}`;
+        //released.textContent = `Released on ${movie.Released}`;
+
+        //make time pretty
+        const formattedRuntime = document.createElement("span");
+        formattedRuntime.classList.add("runtime");
+        const runtime = movie.Runtime;
+        const hoursRuntime = Math.floor(runtime / 60);
+        const minRuntime = runtime % 60;
+        formattedRuntime.textContent = `Runtime: ${hoursRuntime}h ${minRuntime}min`;
+        timeInfo.append(formattedRuntime, released);
+
+
+
+        const metascore = document.createElement("span");
+        metascore.textContent = `Metascore: ${movie.Metascore}%`;
+        metascore.classList.add("metascore");
+        const imdbRating = document.createElement("span");
+        imdbRating.classList.add("imdbRating")
+        imdbRating.textContent = `ImdbRating: ${movie.imdbRating}/10`;
+        rating.append(metascore, imdbRating);
+
+        //const genres = document.createElement("span");
+        //genres.classList.add("genres");
+        //genres.textContent = movie.Genres;    
+
+        const genreContainer = document.createElement("div");
+        genreContainer.classList.add("genreContainer");
+
+        movie.Genres.forEach(genre => {
+          const span = document.createElement("span");
+          span.textContent = genre;
+          span.classList.add("genre");
+          genreContainer.append(span);
+        });
+
+
+        article.appendChild(image);
+        informationen.append(titleContainer, timeInfo, genreContainer);
+        //makeList("Genres: ", movie.Genres, informationen);
+        informationen.append(plot, rating);
+        makeList("Directors: ", movie.Directors, informationen);
+        makeList("Writers: ", movie.Writers, informationen);
+        makeList("Actors: ", movie.Actors, informationen);
+
+        article.appendChild(informationen);
+
+        mainElement.appendChild(article);
+        
       }
 
     } else {
       bodyElement.append(
         "Daten konnten nicht geladen werden, Status " +
-          xhr.status +
-          " - " +
-          xhr.statusText
+        xhr.status +
+        " - " +
+        xhr.statusText
       );
     }
   };
--- server/movie-model.js
+++ server/movie-model.js
@@ -1,2 +1,62 @@
 /* Task 1.1. Add your movie data here 
    and export it so it's available in server.js */
+const movies = {
+   tt0133240: {
+   imdbID:"tt0133240",
+   "Title":"Treasure Planet",
+   "Released":"2002-11-29",
+   "Runtime": 95,
+   "Genres": ["Animation", "Action", "Adventure"],
+   "Directors":["Ron Clements", "John Musker"],
+   "Writers": ["Ron Clements", "John Musker", "Rob Edwards"],
+   "Actors": ["Joseph Gordon-Levitt", "Emma Thompson", "Martin Short"],
+   "Plot": "Jim Hawkins is a teenager who finds the map of a great treasure hidden by a space pirate. Together with some friends, he sets off in a large spaceship, shaped like a caravel, on his quest.",
+   "Poster": "https://m.media-amazon.com/images/M/MV5BN2VjNjk5MTAtNDE3Ni00NjczLWFhN2EtNGQ1ZmZmOWRjZjIyXkEyXkFqcGc@._V1_SX300.jpg",
+   "Metascore": 60,
+   "imdbRating": 7.2
+   },
+   tt1446192: {
+   "imdbID":"tt1446192",
+   "Title":"Rise of the Guardians",
+   "Released":"2012-11-21",
+   "Runtime": 97,
+   "Genres": ["Animation", "Action", "Adventure"],
+   "Directors": ["Peter Ramsey"],
+   "Writers": ["David Lindsay-Abaire", "William Joyce", "Chris Appelhans"],
+   "Actors": ["Hugh Jackman", "Alec Baldwin", "Isla Fisher"],
+   "Plot": "When the evil spirit Pitch launches an assault on Earth, the Immortal Guardians team up to protect the innocence of children all around the world.",
+   "Poster":"https://m.media-amazon.com/images/M/MV5BMTkzMjgwMDg1M15BMl5BanBnXkFtZTcwMTgzNTI1OA@@._V1_SX300.jpg",
+   "Metascore": 58,
+   "imdbRating": 7.2
+   },
+   tt4729430: {
+   "imdbID":"tt4729430",
+   "Title":"Klaus",
+   "Released":"2019-11-15",
+   "Runtime": 96,
+   "Genres": ["Animation", "Adventure", "Comedy"],
+   "Directors": ["Sergio Pablos", "Carlos Martínez López"],
+   "Writers": ["Sergio Pablos", "Jim Mahoney", "Zach Lewis"],
+   "Actors": ["Jason Schwartzman", "J.K. Simmons", "Rashida Jones"],
+   "Plot": "A simple act of kindness always sparks another, even in a frozen, faraway place. When Smeerensburg's new postman, Jesper, befriends toymaker Klaus, their gifts melt an age-old feud and deliver a sleigh full of holiday traditions.",
+   "Poster":"https://m.media-amazon.com/images/M/MV5BZDA4ODhlMzctNGRmYi00NmViLTkxZGYtZjRkNGQ5YzYwYWVlXkEyXkFqcGc@._V1_SX300.jpg",
+   "Metascore": 65,
+   "imdbRating": 8.2
+   },
+   tt5198068: {
+   "imdbID":"tt5198068",
+   "Title":"Wolfwalkers",
+   "Released":"2020-11-13",
+   "Runtime":103,
+   "Genres":["Animation", "Action", "Adventure"],
+   "Directors":["Tomm Moore", "Ross Stewart"],
+   "Writers": ["Will Collins", "Tomm Moore", "Ross Stewart"],
+   "Actors":["Honor Kneafsey", "Eva Whittaker", "Sean Bean"],
+   "Plot":"A young apprentice hunter and her father journey to Ireland to help wipe out the last wolf pack. But everything changes when she befriends a free-spirited girl from a mysterious tribe rumored to transform into wolves by night.",
+   "Poster":"https://m.media-amazon.com/images/M/MV5BYzg3MjBkYmYtZjZkZS00YWVkLTk3YjMtNDBlMTVmNDZkZDJhXkEyXkFqcGc@._V1_SX300.jpg",
+   "Metascore": 87,
+   "imdbRating": 8.0
+   }
+}
+module.exports.movies = movies;
+console.log(module);
